a6b31b1c707b5363a1ac990c6aca91c15543e5bf
rickywang199 <rickywang199@gmail.com>
2026-01-05 15:36:22 -0500

    implemented adapter and provider for auth.ts

---
+++ src/auth.ts
@@ -0,0 +1,15 @@
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/lib/db";
+
+export const { auth, handlers, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  session: { strategy: "database" },
+  providers: [
+    GitHub({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    }),
+  ],
+});
